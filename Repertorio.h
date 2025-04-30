401f714c54a621eab5f92eacd655ff2cc251b196
ajhidrovo <ajhidrovo@espe.edu.ec>
2025-04-30 00:21:56 -0500

    Update Repertorio.h

--- Repertorio.h
+++ Repertorio.h
@@ -7,74 +7,81 @@
 
 using namespace std;
 
-void hello(){
-cout << "----------------" << endl;
-cout << "Hello World" << endl;
-cout << "----------------" << endl;
+void hello() {
+    cout << "----------------" << endl;
+    cout << "Hello World" << endl;
+    cout << "----------------" << endl;
 }
 
-void Suma(){
-int n1, n2, result;
-cout << "Se sumaran 2 numeros a ingresar" << endl;
-cout << "-------------------------------" << endl;
-cout << "Ingrese numero 1 a sumar: " << endl;
-cin >> n1;
-cout << "Ingrese numero 2 a sumar:" << endl;
-cin >> n2;
-result = n1 + n2;
-cout << "Su resultado es: " << result << endl;
+void Suma() {
+    int n1, n2, result;
+    cout << "Se sumaran 2 numeros a ingresar" << endl;
+    cout << "-------------------------------" << endl;
+    cout << "Ingrese numero 1 a sumar: " << endl;
+    cin >> n1;
+    cout << "Ingrese numero 2 a sumar:" << endl;
+    cin >> n2;
+    result = n1 + n2;
+    cout << "Su resultado es: " << result << endl;
 }
 
-void opciones(){
-int op;
- do{
-     cout << " -->> Menu de opciones <<--" << endl;
-         cout << "1. Hello World" << endl;
-             cout << "2. Suma de dos numeros" << endl;
-                 cout << "3.Comparar dos números mayor o menor " << endl;
-                     cout << "4.Estadísticas (n números) " << endl;
-                         cout << "5. FizzBuzz" << endl;
-                             cout << "6.  Salir" << endl;
-                                 cin >> op;
-                                 switch(op){
-                                 case 1:
-                                 cout << "--------------------" << endl;
-                                 cout << "Hecho por JZambrano" << endl;
-                                 cout << "--------------------" << endl;
-                                     hello();
-                                         break;
-                                         case 2:
-                                         cout << "--------------------" << endl;
-                                         cout << "Hecho por JZambrano" << endl;
-                                         cout << "--------------------" << endl;
-                                             Suma();
-                                                 break;
-                                                 case 3:
-                                                  int n1,n2;
-                                             cout<<"---------COMPARAR DOS NÚMEROS--------"<<endl;
-                                             cout<<"Ingrese el primer numero: ";
-                                             cin>>n1;
-                                             cout<<"Ingrese el segundo numero: ";
-                                             cin>>n2;
-                                            if(n1<n2){
-                                            cout<<"El numero "<<n1<<" es menor "<<n2<<endl;
-                                             }else{
-                                             cout<<"El numero "<<n1<<" es mayor "<< n2<<endl;
-                                             }
-                                                     break;
-            case 4:
+void opciones() {
+    int op;
+    do {
+        cout << " -->> Menu de opciones <<--" << endl;
+        cout << "1. Hello World" << endl;
+        cout << "2. Suma de dos numeros" << endl;
+        cout << "3. Comparar dos numeros mayor o menor " << endl;
+        cout << "4. Estadisticas (n numeros) " << endl;
+        cout << "5. FizzBuzz" << endl;
+        cout << "6. Salir" << endl;
+        cin >> op;
+
+        switch (op) {
+            case 1:
+                cout << "--------------------" << endl;
+                cout << "Hecho por JZambrano" << endl;
+                cout << "--------------------" << endl;
+                hello();
+                break;
+
+            case 2:
+                cout << "--------------------" << endl;
+                cout << "Hecho por JZambrano" << endl;
+                cout << "--------------------" << endl;
+                Suma();
+                break;
+
+            case 3: {
+                int n1, n2;
+                cout << "---------COMPARAR DOS NÚMEROS--------" << endl;
+                cout << "Ingrese el primer numero: ";
+                cin >> n1;
+                cout << "Ingrese el segundo numero: ";
+                cin >> n2;
+                if (n1 < n2) {
+                    cout << "El numero " << n1 << " es menor que " << n2 << endl;
+                } else if (n1 > n2) {
+                    cout << "El numero " << n1 << " es mayor que " << n2 << endl;
+                } else {
+                    cout << "Ambos números son iguales." << endl;
+                }
+                break;
+            }
+
+            case 4: {
                 cout << "--------------------" << endl;
                 cout << "Hecho por AjHidrovo" << endl;
                 cout << "--------------------" << endl;
 
                 int n;
                 // Solicitar al usuario la cantidad de números
-                cout << "Introduce la cantidad de números: ";
+                cout << "Introduce la cantidad de numeros: ";
                 cin >> n;
 
                 // Validar que el número de entradas sea válido
                 if (n <= 0) {
-                    cout << "El número de entradas debe ser mayor que 0." << endl;
+                    cout << "El numero de entradas debe ser mayor que 0." << endl;
                     break;
                 }
 
@@ -83,13 +90,13 @@ int op;
                 double maximo, minimo;
 
                 // Leer el primer número para inicializar maximo y minimo
-                cout << "Introduce el primer número: ";
+                cout << "Introduce el primer numero: ";
                 cin >> numero;
                 maximo = minimo = numero;  // Inicializamos ambos con el primer número
 
                 // Leer los siguientes números y calcular las estadísticas
                 for (int i = 1; i < n; i++) {
-                    cout << "Introduce el siguiente número: ";
+                    cout << "Introduce el siguiente numero: ";
                     cin >> numero;
 
                     // Sumar los números para calcular el promedio
@@ -111,17 +118,18 @@ int op;
                 double promedio = suma / n;
 
                 // Mostrar los resultados
-                cout << "\nEstadísticas:\n";
+                cout << "\nEstadisticas:\n";
                 cout << "Promedio: " << promedio << endl;
-                cout << "Valor máximo: " << maximo << endl;
-                cout << "Valor mínimo: " << minimo << endl;
+                cout << "Valor maximo: " << maximo << endl;
+                cout << "Valor minimo: " << minimo << endl;
                 break;
-                                                        
-            case 5:
+            }
+
+            case 5: {
                 cout << "--------------------" << endl;
                 cout << "Hecho por AjHidrovo" << endl;
                 cout << "--------------------" << endl;
-                
+
                 // Imprime los números del 1 al 100
                 cout << "Ejecutando FizzBuzz:" << endl;
                 for (int i = 1; i <= 100; i++) {
@@ -145,16 +153,17 @@ int op;
                     cout << endl;
                 }
                 break;
-                                                          case 6:
-                                                      cout << "Saliendo del programa" << endl;
-                                                          break;
-                                                                 default:
-                                                                     cout << "Error, intente nuevamente" << endl;
-                                                                         break;
+            }
 
+            case 6:
+                cout << "Saliendo del programa" << endl;
+                break;
 
-                                                                         }
-                                                                         }while(op!=6);
+            default:
+                cout << "Error, intente nuevamente" << endl;
+                break;
+        }
+    } while (op != 6);
+}
 
-                                                                         }
-                                                                         #endif // REPOSITORIO_H_INCLUDED
+#endif // REPOSITORIO_H_INCLUDED
